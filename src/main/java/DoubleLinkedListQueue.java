d6bad65335a5f52f6bf3c08cf8cdbd4ccd082362
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-03-15 16:57:56 +0100

    fix: Double linked list did not append or remove items correctly

--- src/main/java/DoubleLinkedListQueue.java
+++ src/main/java/DoubleLinkedListQueue.java
@@ -8,22 +8,77 @@ public class DoubleLinkedListQueue<T> implements DoubleEndedQueue<T> {
 
     @Override
     public void append(DequeNode<T> node) {
-        appendNode(last,node);
+        if (node == null) throw new IllegalArgumentException("Argument `node` cannot be null");
+        if (node.getPrevious() != null || node.getNext() != null)
+            throw new IllegalStateException("The node is already inserted on a list");
+
+        if (size == 0) {
+            first = node;
+        } else {
+            node.setPrevious(last);
+            last.setNext(node);
+        }
+
+        last = node;
+        size++;
     }
 
     @Override
     public void appendLeft(DequeNode<T> node) {
-        appendNode(first,node);
+        if (node == null) throw new IllegalArgumentException("Argument `node` cannot be null");
+        if (node.getPrevious() != null || node.getNext() != null)
+            throw new IllegalStateException("The node is already inserted on a list");
+
+        if (size == 0) {
+            last = node;
+        } else {
+            node.setNext(first);
+            first.setPrevious(node);
+        }
+        first = node;
+        size++;
     }
 
     @Override
     public void deleteFirst() {
-        deleteNode(first);
+        if (size == 0)
+            throw new IllegalStateException("Cannot delete last element on empty queue");
+
+        var deletedNode = first;
+
+        if (size == 1) {
+            first = null;
+            last = null;
+        } else {
+            var second = first.getNext();
+            second.setPrevious(null);
+            first = second;
+        }
+
+        deletedNode.setNext(null);
+        deletedNode.setPrevious(null);
+        size--;
     }
 
     @Override
     public void deleteLast() {
-        deleteNode(last);
+        if (size == 0)
+            throw new IllegalStateException("Cannot delete last element on empty queue");
+
+        var deletedNode = last;
+
+        if (size == 1) {
+            first = null;
+            last = null;
+        } else {
+            var nextToLast = last.getPrevious();
+            nextToLast.setNext(null);
+            last = nextToLast;
+        }
+
+        deletedNode.setNext(null);
+        deletedNode.setPrevious(null);
+        size--;
     }
 
     @Override
@@ -39,58 +94,5 @@ public class DoubleLinkedListQueue<T> implements DoubleEndedQueue<T> {
     @Override
     public int size() {
         return this.size;
-    }
-
-    private void deleteNode(DequeNode<T> node) {
-        if (size == 0)
-            throw new IllegalStateException("Cannot delete last element on empty queue");
-        if (node == null)
-            throw new IllegalArgumentException("Argument `node` cannot be null");
-        if (!this.contains(node))
-            throw new IllegalArgumentException("The node is not contained on this list");
-
-        if (size == 1) {
-            first = null;
-            last = null;
-        } else {
-            // size > 1
-            var previous = node.getPrevious();
-            var next = node.getNext();
-
-            if (previous != null) previous.setNext(next);
-            if (next != null) next.setPrevious(previous);
-
-            node.setPrevious(null);
-            node.setNext(null);
-        }
-        size--;
-    }
-
-    private boolean contains(DequeNode<T> node) {
-        if (first == null) return false;
-
-        var iterable = first;
-        while (!first.isLastNode()) {
-            if(iterable.equals(node)) return true;
-            iterable = iterable.getNext();
-        }
-
-        return false;
-    }
-
-    private void appendNode(DequeNode<T> list, DequeNode<T> node) {
-        if (node == null) throw new IllegalArgumentException("Argument `node` cannot be null");
-        if (list == null) throw new IllegalArgumentException("Argument `list` cannot be null");
-        if (node.getPrevious() != null || node.getNext() != null)
-            throw new IllegalStateException("The node is already inserted on a list");
-
-        var next = list.getNext();
-
-        list.setNext(node);
-        node.setPrevious(list);
-        node.setNext(next);
-        if (next != null) next.setPrevious(node);
-
-        size++;
     }
 }
