622518b0ba0b84ad3fb2f42025d5c9bd7cfc06b6
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-03-15 11:01:41 +0100

    new: DoubleLinkedListQueue

---
+++ src/main/java/DoubleLinkedListQueue.java
@@ -0,0 +1,36 @@
+public class DoubleLinkedListQueue<T> implements DoubleEndedQueue<T>{
+    @Override
+    public void append(DequeNode<T> node) {
+
+    }
+
+    @Override
+    public void appendLeft(DequeNode<T> node) {
+
+    }
+
+    @Override
+    public void deleteFirst() {
+
+    }
+
+    @Override
+    public void deleteLast() {
+
+    }
+
+    @Override
+    public DequeNode<T> peekFirst() {
+        return null;
+    }
+
+    @Override
+    public DequeNode<T> peekLast() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
