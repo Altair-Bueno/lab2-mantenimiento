7556f2d353d77c2628b73d199c8f4b76e6dc6d96
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-03-15 11:50:15 +0100

    new: DoubleLinkedListQueue implementation

--- src/main/java/DoubleLinkedListQueue.java
+++ src/main/java/DoubleLinkedListQueue.java
@@ -41,4 +41,56 @@ public class DoubleLinkedListQueue<T> implements DoubleEndedQueue<T> {
         return this.size;
     }
 
-   
+    private void deleteNode(DequeNode<T> node) {
+        if (size == 0)
+            throw new IllegalStateException("Cannot delete last element on empty queue");
+        if (node == null)
+            throw new IllegalArgumentException("Argument `node` cannot be null");
+        if (!this.contains(node))
+            throw new IllegalArgumentException("The node is not contained on this list");
+
+        if (size == 1) {
+            first = null;
+            last = null;
+        } else {
+            // size > 1
+            var previous = node.getPrevious();
+            var next = node.getNext();
+
+            if (previous != null) previous.setNext(next);
+            if (next != null) next.setPrevious(previous);
+
+            node.setPrevious(null);
+            node.setNext(null);
+        }
+        size--;
+    }
+
+    private boolean contains(DequeNode<T> node) {
+        if (first == null) return false;
+
+        var iterable = first;
+        while (!first.isLastNode()) {
+            if(iterable.equals(node)) return true;
+            iterable = iterable.getNext();
+        }
+
+        return false;
+    }
+
+    private void appendNode(DequeNode<T> list, DequeNode<T> node) {
+        if (node == null) throw new IllegalArgumentException("Argument `node` cannot be null");
+        if (list == null) throw new IllegalArgumentException("Argument `list` cannot be null");
+        if (node.getPrevious() != null || node.getNext() != null)
+            throw new IllegalStateException("The node is already inserted on a list");
+
+        var next = list.getNext();
+
+        list.setNext(node);
+        node.setPrevious(list);
+        node.setNext(next);
+        if (next != null) next.setPrevious(node);
+
+        size++;
+    }
+}
