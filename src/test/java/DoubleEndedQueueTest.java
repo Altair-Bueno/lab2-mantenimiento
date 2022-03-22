6f6caef5b727312761489763ae769d6665f25549
carmengo01 <carmela050701@gmail.com>
2022-03-22 12:12:56 +0100

    implemented find and sort tests

--- src/test/java/DoubleEndedQueueTest.java
+++ src/test/java/DoubleEndedQueueTest.java
@@ -5,12 +5,19 @@ import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Comparator;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DoubleEndedQueueTest {
     public DoubleEndedQueue<Integer> queue;
+    private Comparator<DequeNode<Integer>> comparator = new Comparator<DequeNode<Integer>>() {
+        @Override
+        public int compare(DequeNode<Integer> o1, DequeNode<Integer> o2) {
+            return o1.getItem()-o2.getItem();
+        }
+    };
 
     @BeforeEach
     public void setup() {
@@ -258,4 +265,68 @@ class DoubleEndedQueueTest {
         assertThrows(IllegalStateException.class,lambda);
     }
 
+    // DoubleEndedQueue(DequeNode(1))
+    // find(1) -> DequeNode(1)
+    @Test
+    public void findShouldReturnTheNodeWithTheGivenItem(){
+        DequeNode<Integer> expectedValue = new DequeNode<>(1, null, null);
+        queue.append(expectedValue);
+        DequeNode<Integer> obtainedValue=queue.find(1);
+        assertEquals(expectedValue,obtainedValue);
+    }
+
+    // DoubleEndedQueue()
+    // find(1) -> null
+    @Test
+    public void findOfAnItemThatIsNotInTheQueueShouldReturnNull(){
+        assertNull(queue.find(1));
+    }
+
+    // DoubleEndedQueue()
+    // find(null) -> IllegalArgumentException
+    @Test
+    public void findANullShouldRaiseAnException(){
+        Executable lambda = ()-> queue.find(null);
+        assertThrows(IllegalArgumentException.class,lambda);
+    }
+
+    // DoubleEndedQueue(DequeNode(2),DequeNode(3),DequeNode(1))
+    // sort(comparator) -> DoubleEndedQueue(DequeNode(1),DequeNode(2),DequeNode(3))
+    @Test
+    public void sortOfADisorderedQueueShouldOrderIt(){
+        DequeNode<Integer> node1 = new DequeNode<>(2, null, null);
+        DequeNode<Integer> node2 = new DequeNode<>(3, null, null);
+        DequeNode<Integer> node3 = new DequeNode<>(1, null, null);
+        queue.append(node1);
+        queue.append(node2);
+        queue.append(node3);
+
+        queue.sort(comparator);
+        assertEquals(node3,queue.peekFirst());
+        assertEquals(node2,queue.peekLast());
+    }
+
+    // DoubleEndedQueue(DequeNode(1),DequeNode(2),DequeNode(3))
+    // sort(comparator) -> DoubleEndedQueue(DequeNode(1),DequeNode(2),DequeNode(3))
+    @Test
+    public void sortOfAnOrderedQueueShouldDoNothing(){
+        DequeNode<Integer> node1 = new DequeNode<>(1, null, null);
+        DequeNode<Integer> node2 = new DequeNode<>(2, null, null);
+        DequeNode<Integer> node3 = new DequeNode<>(3, null, null);
+        queue.append(node1);
+        queue.append(node2);
+        queue.append(node3);
+
+        queue.sort(comparator);
+        assertEquals(node1,queue.peekFirst());
+        assertEquals(node3,queue.peekLast());
+    }
+
+    // DoubleEndedQueue()
+    // sort(comparator) -> size() = 0
+    @Test
+    public void sortOfAnEmptyQueueShouldDoNothing(){
+        queue.sort(comparator);
+        assertEquals(0,queue.size());
+    }
 }
